7f472d8cdf5fe4ab2d8873ceb1197d61f1398a1f
Artur Mukhametov <otejio@gmail.com>
2022-02-08 21:06:45 +0300

    complete UserEmail task

--- 3-homework/readme.md
+++ 3-homework/readme.md
@@ -1,4 +1,5 @@
 # 1 task type UserEmail
 Создать брендированный тип UserEmail, который обязательно должен быть не пустым + быть email + иметь постфикс “@itkachalka.ru”
+[код](user-email.ts)
 
 # 2 task online store
--- 3-homework/user-email.ts
+++ 3-homework/user-email.ts
@@ -1,4 +1,26 @@
-type UserEmail = {
+type User = {
     id: string
-    email: string
+    email: UserEmail
+}
+
+type UserEmail = string & { readonly UserEmail: unique symbol }
+
+const UserEmail = {
+    ofString: (value: string) => {
+        if(!value){
+            throw new Error(`${value} must be not empty`)
+        }
+        if(!(/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(value))){
+            throw new Error(`${value} must be correct email`)
+        }
+        if(!(/@itkachalka.ru\s*$/.test(value))){
+            throw new Error(`${value} must be domain @itkachalka.ru`)
+        }
+        return value as UserEmail
+    }
+}
+
+const user: User = {
+    id: "id",
+    email: UserEmail.ofString("am@zen.car")
 }
